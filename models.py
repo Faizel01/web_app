e238c08de7ee63ecb1bf17d7d9f026b369c89db5
Faizel01 <e.sachenkov@gmail.com>
2023-07-28 16:53:03 +0300

    Зарегестрировал в файл settings.py приложение main; добавил миграции

--- models.py
+++ models.py
@@ -6,3 +6,18 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+
+
+class ClientInfo(models.Model):
+
+    client_id = models.AutoField(primary_key=True)
+    first_name = models.TextField()
+    last_name = models.TextField()
+    phone_number = models.DecimalField(max_digits=10, decimal_places=0)
+    problems = models.CharField(max_length=250)
+
+    class Meta:
+        db_table = 'client_info'
+
+
+
--- website/settings.py
+++ website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'infodb',
+        'USER': 'postgres',
+        'PASSWORD': 'E200619103511S',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
